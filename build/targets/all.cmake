c982d7e8bde97a4682022f69940baacdf5ac8791
Domenico Cipriani <77632814+lucretiomsp@users.noreply.github.com>
2024-01-19 15:36:12 +0100

    Update all.cmake
    include just faustLib, not OSC and http

--- build/targets/all.cmake
+++ build/targets/all.cmake
@@ -5,11 +5,11 @@ set ( INCLUDE_EXECUTABLE  ON  CACHE STRING  "Include Faust compiler" FORCE )
 set ( INCLUDE_STATIC      ON  CACHE STRING  "Include static Faust library" FORCE )
 set ( INCLUDE_DYNAMIC     ON  CACHE STRING  "Include dynamic Faust library" FORCE )
 
-set ( INCLUDE_OSC         ON  CACHE STRING  "Include Faust OSC static library" FORCE )
-set ( INCLUDE_HTTP        ON  CACHE STRING  "Include Faust HTTPD static library" FORCE )
+set ( INCLUDE_OSC         OFF  CACHE STRING  "Include Faust OSC static library" FORCE )
+set ( INCLUDE_HTTP        OFF CACHE STRING  "Include Faust HTTPD static library" FORCE )
 
-set ( OSCDYNAMIC          ON  CACHE STRING  "Include Faust OSC dynamic library" FORCE )
-set ( HTTPDYNAMIC         ON  CACHE STRING  "Include Faust HTTP dynamic library" FORCE )
+set ( OSCDYNAMIC          OFF  CACHE STRING  "Include Faust OSC dynamic library" FORCE )
+set ( HTTPDYNAMIC         OFF  CACHE STRING  "Include Faust HTTP dynamic library" FORCE )
 
 set ( INCLUDE_ITP         ON  CACHE STRING  "Include Faust Machine library" FORCE )
 set ( ITPDYNAMIC          ON  CACHE STRING  "Include Faust Machine library" FORCE )
