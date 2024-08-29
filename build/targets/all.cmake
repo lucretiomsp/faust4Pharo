fac7d11624d5e1b9ab477e96e5cee592ae721c4e
Domenico Cipriani <77632814+lucretiomsp@users.noreply.github.com>
2024-08-29 09:25:34 +0200

    Update all.cmake
    disabled OSC and HHTTP

--- build/targets/all.cmake
+++ build/targets/all.cmake
@@ -5,8 +5,8 @@ set ( INCLUDE_EXECUTABLE  ON  CACHE STRING  "Include Faust compiler" FORCE )
 set ( INCLUDE_STATIC      ON  CACHE STRING  "Include static Faust library" FORCE )
 set ( INCLUDE_DYNAMIC     ON  CACHE STRING  "Include dynamic Faust library" FORCE )
 
-set ( INCLUDE_OSC         ON  CACHE STRING  "Include Faust OSC static library" FORCE )
-set ( INCLUDE_HTTP        ON  CACHE STRING  "Include Faust HTTPD static library" FORCE )
+set ( INCLUDE_OSC         OFF  CACHE STRING  "Include Faust OSC static library" FORCE )
+set ( INCLUDE_HTTP        OFF  CACHE STRING  "Include Faust HTTPD static library" FORCE )
 
 set ( OSCDYNAMIC          ON  CACHE STRING  "Include Faust OSC dynamic library" FORCE )
 set ( HTTPDYNAMIC         ON  CACHE STRING  "Include Faust HTTP dynamic library" FORCE )
