55662ec12f8dfaacdb950d94ffc7c07e639fb8c7
Domenico Cipriani <77632814+lucretiomsp@users.noreply.github.com>
2024-09-01 14:59:20 +0200

    Update regular.cmake

--- build/backends/regular.cmake
+++ build/backends/regular.cmake
@@ -16,7 +16,7 @@ set ( CMAJOR_BACKEND COMPILER STATIC DYNAMIC WASM CACHE STRING  "Include Cmajor 
 set ( CSHARP_BACKEND COMPILER STATIC DYNAMIC      CACHE STRING  "Include CSharp backend" FORCE )
 set ( DLANG_BACKEND  COMPILER STATIC DYNAMIC      CACHE STRING  "Include Dlang backend" FORCE )
 set ( FIR_BACKEND                        OFF      CACHE STRING  "Include FIR backend" FORCE )
-set ( INTERP_BACKEND                     OFF      CACHE STRING  "Include Interpreter backend" FORCE )
+set ( INTERP_BACKEND COMPILER STATIC DYNAMIC       CACHE STRING  "Include Interpreter backend" FORCE )
 set ( JAVA_BACKEND   COMPILER STATIC DYNAMIC      CACHE STRING  "Include JAVA backend" FORCE )
 set ( JAX_BACKEND    COMPILER STATIC DYNAMIC      CACHE STRING  "Include JAX backend"  FORCE )
 set ( JULIA_BACKEND  COMPILER STATIC DYNAMIC      CACHE STRING  "Include Julia backend" FORCE )
