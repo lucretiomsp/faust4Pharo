9c5c1ab3c97c7f2de7318c52e1064bee2a230406
Domenico Cipriani <77632814+lucretiomsp@users.noreply.github.com>
2024-09-01 14:57:27 +0200

    Update osx.cmake

--- build/targets/osx.cmake
+++ build/targets/osx.cmake
@@ -3,8 +3,8 @@
 # it includes MacOS Specific settings
 
 set ( INCLUDE_EXECUTABLE  ON  CACHE STRING  "Include Faust compiler" FORCE )
-set ( INCLUDE_STATIC      OFF CACHE STRING  "Include static Faust library" FORCE )
-set ( INCLUDE_DYNAMIC     OFF CACHE STRING  "Include dynamic Faust library" FORCE )
+set ( INCLUDE_STATIC      ON CACHE STRING  "Include static Faust library" FORCE )
+set ( INCLUDE_DYNAMIC     ON CACHE STRING  "Include dynamic Faust library" FORCE )
 
 set ( INCLUDE_OSC         ON  CACHE STRING  "Include Faust OSC static library" FORCE )
 set ( INCLUDE_HTTP        ON  CACHE STRING  "Include Faust HTTPD static library" FORCE )
@@ -15,6 +15,6 @@ set ( HTTPDYNAMIC         OFF CACHE STRING  "Include Faust HTTP dynamic library"
 set ( INCLUDE_ITP         OFF  CACHE STRING  "Include Faust Machine library" FORCE )
 set ( ITPDYNAMIC          OFF  CACHE STRING  "Include Faust Machine library" FORCE )
 
-set ( UNIVERSAL 	      OFF CACHE STRING  "Compiles and combines i386 and x86_64 architectures" FORCE )
+set ( UNIVERSAL 	      ON CACHE STRING  "Compiles and combines i386 and x86_64 architectures" FORCE )
 set ( DEPLOYMENT_TARGET   ON  CACHE STRING  "Control MacOS deployment target settings" FORCE )
 set ( DEPLOYMENT_TARGET_VERSION 10.11 CACHE STRING "Sets deployment target version (unused when DEPLOYMENT_TARGET is off)" FORCE )
